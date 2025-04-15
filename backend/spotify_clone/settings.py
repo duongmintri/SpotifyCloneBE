6e0b5f3da107ad175fd319ffa513c936c4fc8d4b
LuuD04N <doanphongluu082@gmail.com>
2025-04-15 08:19:56 +0700

    Cài đặt môi trường django và database trong setting

--- backend/spotify_clone/settings.py
+++ backend/spotify_clone/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'spotify_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spotify_clone',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
